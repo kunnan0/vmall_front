1bfea92690cce9a78fb3c23e74bec7bd7d738f42
KuRN <kurunnan@qq.com>
2021-01-21 09:21:40 +0800

    jiandan_luyou_over

---
+++ src/router/dist/index.dev.js
@@ -0,0 +1,42 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _vueRouter = _interopRequireDefault(require("vue-router"));
+
+var _Index = _interopRequireDefault(require("@/views/Index.vue"));
+
+var _Home = _interopRequireDefault(require("@/views/Home"));
+
+var _Login = _interopRequireDefault(require("@/views/Login"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_vueRouter["default"]);
+
+var routes = [{
+  path: '/',
+  redirect: '/home',
+  name: 'home',
+  component: _Index["default"],
+  children: [{
+    path: 'home',
+    component: _Home["default"]
+  }]
+}, {
+  path: '/login',
+  name: 'login',
+  component: _Login["default"]
+}];
+var router = new _vueRouter["default"]({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
+});
+var _default = router;
+exports["default"] = _default;
---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- 导航栏 -->
+        <m-header></m-header>
+        <!-- 其他首页内容 -->
+        <router-view></router-view>
+
+    </div>
+</template>
+
+<script>
+    import MHeader from '@/common/MHeader.vue';
+    export default {
+        components:{
+            MHeader,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
